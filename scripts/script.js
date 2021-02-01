0fdc9d21ad9f0561640e3f5950fa3f53b11d155e
Filip Krzyzanowski <filip.krzyzanowski@protonmail.com>
2021-02-01 18:10:30 +0100

    Dodanie wszystkich kart, przełączanie ich

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+let cards = document.getElementsByClassName("cards");
+let about = document.getElementById("about");
+let resume = document.getElementById("resume");
+let portfolio = document.getElementById("portfolio");
+let contact = document.getElementById("contact");
+
+function cardSwitcher(id) {
+
+    for(let i=0; i<cards.length; i++) {
+        cards[i].style.display = "none";
+    }
+
+    switch(id) {
+        case 1:
+            about.classList += " active";
+            about.style.display = "grid";
+            break;
+        case 2:
+            resume.style.display = "grid";
+            break;
+        case 3:
+            portfolio.style.display = "grid";
+            break;
+        case 4:
+            contact.style.display = "grid";
+            break;
+    }
+}
