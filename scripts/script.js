c9bc3815362d492831465b08f390f076409f4b85
Filip Krzyzanowski <filip.krzyzanowski@protonmail.com>
2021-02-09 16:44:55 +0100

    Dodanie animacji zmiany karty

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,69 @@
 let cards = document.getElementsByClassName("cards");
+let card = document.getElementsByClassName("card");
 let about = document.getElementById("about");
 let resume = document.getElementById("resume");
 let portfolio = document.getElementById("portfolio");
 let contact = document.getElementById("contact");
+let active = 1;
+let last = 1
 
 function cardSwitcher(id) {
+    if (active != id) {
+        cards[id-1].style.zIndex = "0";
+        switch(id) {
+            case 1:
+                active = id;
+                about.style.zIndex = "1";
+                cardAnimate(0, 1, id);
+                break;
+            case 2:
+                active = id;
+                resume.style.zIndex = "1";
+                cardAnimate(2, 3, id);
+                break;
+            case 3:
+                active = id;
+                portfolio.style.zIndex = "1";
+                cardAnimate(4, 5, id);
+                break;
+            case 4:
+                active = id;
+                contact.style.zIndex = "1";
+                cardAnimate(6, 7, id);
+                break;
+        }
+    }
+}
+
+function cardAnimate(text, photo, id) {
+    card[text].animate([
+        { left: '-50%'},
+        { left: 0 }
+    ], {
+        duration: 2000,
+        iterations: 1,
+        direction: 'alternate',
+        fill: 'forwards'
+    });
+
+    card[photo].animate([
+        { right: '-50%'},
+        { right: 0 }
+    ], {
+        duration: 2000,
+        iterations: 1,
+        direction: 'alternate',
+        fill: 'forwards'
+    });
 
     for(let i=0; i<cards.length; i++) {
-        cards[i].style.display = "none";
+        if (i+1 != id) {
+            cards[i].style.zIndex = "-1";
+        } 
+        if (i+1 == last) {
+            cards[last-1].style.zIndex = "0";
+        }
     }
 
-    switch(id) {
-        case 1:
-            about.style.display = "grid";
-            break;
-        case 2:
-            resume.style.display = "grid";
-            break;
-        case 3:
-            portfolio.style.display = "grid";
-            break;
-        case 4:
-            contact.style.display = "grid";
-            break;
-    }
+    last = id
 }
